aa0442307581601569a83b4ef3907a32712c5a2f
Juan <j.gallego6@utp.edu.co>
2026-08-06 16:14:48 -0500

    Variables de entorno establecidas

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+$connection = pg_connect($conn_string);
+?>
